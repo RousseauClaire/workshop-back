673190cc6f466cebfa3bb6ffe0f58c17c80ed347
Claire <claire.rouss@gmail.com>
2023-04-04 10:18:00 +0200

    user routes + controllers

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -3,11 +3,13 @@ const jwt = require('jsonwebtoken');
 const User = require("../models/User");
 
 exports.register = (req, res, next) => {
+    // On vérifie que l'e-mail ne soit pas déjà présent en base
     User.findOne({email: req.body.email})
         .then(user => {
             if (user) {
                 return res.status(400).json({message: "L'identifiant doit être unique"});
             }})
+    // On crypt le mot de passe
     bcrypt.hash(req.body.password, 10)
         .then(mdpHash => {
             const user = new User({
@@ -33,7 +35,7 @@ exports.login = (req, res, next) => {
                     if (!valid) {
                         return res.status(401).json({message: "Utilisateur ou mot de passe incorrect"});
                     }
-                    // Envoi du token
+                    // Envoi du token et du userId
                     res.status(200).json({
                         userId: user._id,
                         token: jwt.sign(
@@ -52,5 +54,28 @@ exports.login = (req, res, next) => {
 }
 
 exports.getProfile = (req, res, next) => {
+    User.findOne({_id: req.auth.userId})
+        .then(user => res.status(200).json(user))
+        .catch(error => res.status(400).json({error}));
+}
 
+exports.getAllUsers = (req, res, next) => {
+    User.find()
+        .then(user => res.status(200).json(user))
+        .catch(error => res.status(400).json({error}));
+}
+
+exports.getOneUser = (req, res, next) => {
+    User.findOne({_id: req.params.id})
+        .then(user => res.status(200).json(user))
+        .catch(error => res.status(400).json({error}));
+}
+
+exports.modifyUser = (req, res, next) => {
+}
+
+exports.deleteUser = (req, res, next) => {
+    User.deleteOne({_id: req.params.id})
+        .then(() => {res.status(200).json({message : "User supprimé"})})
+        .catch(error => res.status(400).json({error}))
 }
--- routes/user.route.js
+++ routes/user.route.js
@@ -12,4 +12,16 @@ router.put('/login', userController.login);
 // Get self profile informations
 router.get('/profile', auth, userController.getProfile);
 
+// Get all users
+router.get('/', auth, userController.getAllUsers);
+
+// Get an user
+router.get('/:id', auth, userController.getOneUser);
+
+// Modify an user
+router.put('/:id', auth, userController.modifyUser);
+
+// Delete an user
+router.delete('/:id', auth, userController.deleteUser);
+
 module.exports = router;
